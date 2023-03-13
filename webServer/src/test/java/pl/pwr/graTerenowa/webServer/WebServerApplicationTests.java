c3aa802b1d446c561fa11238c5007973ab5135ef
D <259053@student.pwr.edu.pl>
2023-03-13 20:48:24 +0100

    Initial spring configuration commit

---
+++ webServer/src/test/java/pl/pwr/graTerenowa/webServer/WebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pwr.graTerenowa.webServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
