1c6e80fba85e52c9ccb2c25211ee246a06c5449a
matszu187_dpp_2023 <matszu187_dpp_2023@e-science.pl>
2023-04-26 16:04:05 +0200

    add accelerometer to object edit form

--- cz1/app/src/main/java/wro/per/activities/ProfilActivity.java
+++ cz1/app/src/main/java/wro/per/activities/ProfilActivity.java
@@ -1,18 +1,26 @@
 package wro.per.activities;
 
+import static java.lang.Math.abs;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import wro.per.R;
-import wro.per.fragments.MenuFragment;
 
-public class ProfilActivity extends AppCompatActivity {
+public class ProfilActivity extends AppCompatActivity implements SensorEventListener {
 
+    SensorManager sensorManager;
+    Sensor accelerometr;
+
+    private float x, y, z;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,6 +28,40 @@ public class ProfilActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edycja_obiektu_layout);
 
+        Button wykonajZdjecieButton;
+        wykonajZdjecieButton = findViewById(R.id.zdjecie_szczegolu_button);
+        wykonajZdjecieButton.setOnClickListener(view -> pobierzNachylenie());
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometr = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    public void pobierzNachylenie() {
+        if(abs(x) < 1)
+        {
+            System.out.println("X wynosi: "+x);
+            //jest git
+            Toast.makeText(this, "Jest prosto", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometr, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            x = event.values[0];
+            y = event.values[1];
+            z = event.values[2];
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
 }
