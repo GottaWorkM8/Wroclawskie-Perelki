44dded55bf933364ef5ac1bd223dda67c2856a93
wiksys123 <wiksys123_dpp_2023@e-science.pl>
2023-04-28 16:28:06 +0200

    Add view Camera

--- cz1/app/src/main/java/wro/per/activities/ProfilActivity.java
+++ cz1/app/src/main/java/wro/per/activities/ProfilActivity.java
@@ -51,54 +51,13 @@ public class ProfilActivity extends AppCompatActivity implements SensorEventList
         wykonajZdjecieButton = findViewById(R.id.zdjecie_szczegolu_button);
 
         wykonajZdjecieButton.setOnClickListener(view -> {
-                    Intent intent = new Intent(this, CameraActivity.class);
+                    Intent intent = new Intent(this, KameraActivity.class);
                     startActivity(intent);
         });
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         accelerometr = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
     }
-
- /*   private ActivityResultLauncher<Intent> takePictureLauncher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            result -> {
-                if (result.getResultCode() == RESULT_OK) {
-                    Bundle extras = result.getData().getExtras();
-                    Bitmap imageBitmap = (Bitmap) extras.get("data");
-
-                    ImageView imageView = findViewById(R.id.zdjecie_szczegolu_imageview);
-                    imageView.setImageBitmap(imageBitmap);
-
-                    if(abs(x) < 1)
-                    {
-                        System.out.println("X wynosi: "+x);
-                        //jest git
-                        Toast.makeText(this, "Jest prosto", Toast.LENGTH_SHORT).show();
-                    }
-                    // tu należy dodać kod obsługi wykonanego zdjęcia
-                }
-            }
-    );
-
-
-    public void takePicture() {
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-                != PackageManager.PERMISSION_GRANTED) {
-            // Brak uprawnień do aparatu, należy poprosić użytkownika o zgodę
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.CAMERA},
-                    MY_PERMISSIONS_REQUEST_CAMERA);
-        } else {
-            // Wywołaj aktywność aparatu
-            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                takePictureLauncher.launch(takePictureIntent);
-            }
-
-        }
-
-    }*/
 
     @Override
     protected void onResume() {
