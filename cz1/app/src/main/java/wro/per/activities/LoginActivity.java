51ff675c0a610d61067484685ee58039cfd8cbe8
barto <bartoszkipa@gmail.com>
2023-05-16 21:20:36 +0200

    LoginActivity

---
+++ cz1/app/src/main/java/wro/per/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package wro.per.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import wro.per.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
